432e04ec82fa7ec97995945887acf90df8d7692e
Olafur Palsson <olafur.palsson2@gmail.com>
2021-08-29 22:42:04 +0000

    fix typo in comment

--- WowMagic.java
+++ WowMagic.java
@@ -3,7 +3,7 @@ import lol.Dog;
 class WowMagic {
   public static void main(String[] args) {
     Dog dog = new Dog("Mahoe");
-    // Dog.sayHello() doesn't exist, but dog on the "new Dog" or constructed/instantiated dog does
+    // Dog.sayHello() doesn't exist, but "sayHello" on the "new Dog" or constructed/instantiated dog does
     dog.sayHello();
     // Here we call the main method. Because it's a satic method
     // we don't need to create a "new Dog()"
